13de92072013e828a8f28e1069bec9176d6e5ff9
unknown <empregos97@gmail.com>
2024-09-03 15:34:56 -0300

    resume AI implementation

---
+++ actions/resume.tsx
@@ -0,0 +1,28 @@
+"use server";
+
+import { streamText } from "ai";
+import { model } from "./user";
+import { getPromptByRole } from "@/lib/role/getPromptByRole";
+import { createStreamableValue } from "ai/rsc";
+
+export const resume = async (username: string, role: string) => {
+  const { textStream } = await streamText({
+    model: model("gemini-1.5-flash-latest", {
+      safetySettings: [
+        {
+          category: "HARM_CATEGORY_HATE_SPEECH",
+          threshold: "BLOCK_LOW_AND_ABOVE",
+        },
+      ],
+    }),
+    system:
+      "Your answer cannot be more than 4 numbers, always keep that in mind. You are a very accurate and professional assistant evaluator who receives various types of values ​​and gives an evaluation of great quality.",
+    prompt: getPromptByRole(role as any, ""),
+  });
+
+  const result = createStreamableValue(textStream);
+
+  return {
+    resume: result.value,
+  };
+};
